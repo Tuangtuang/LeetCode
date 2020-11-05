1dde4184594a81af3d5fb338cc82b3d08f202954
Tuangtuang <tyqbest@163.com>
2020-11-05 12:12:11 +0800

    tree in order

--- src/QueueStack/QueueStackSolution.java
+++ src/QueueStack/QueueStackSolution.java
@@ -416,4 +416,23 @@ public class QueueStackSolution {
         DFSfindTargetSum(nums,sum,depth,curSum);
         return count;
     }
+
+
+//    Binary Tree Inorder Traversal
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List < Integer > res = new ArrayList < > ();
+        Stack < TreeNode > stack = new Stack < > ();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            res.add(curr.val);
+            curr = curr.right;
+        }
+        return res;
+
+    }
 }
